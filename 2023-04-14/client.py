8bc40a66d66deeb52497e8a50d29e39c2d8b7574
Alex Carrega <contact@alexcarrega.com>
2023-04-14 15:20:05 +0200

    Minor changes

--- 2023-04-14/client.py
+++ 2023-04-14/client.py
@@ -1,36 +1,42 @@
+import logging
 import socket
 import sys
+from threading import Thread
 
 import yaml
 
 
 def main():
-    try: 
+    try:
         # create a TCP socket (SOCK_STREAM)
-        s = socket.socket(family=socket.AF_INET,
-                          type=socket.SOCK_STREAM, proto=0)
+        s = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM, proto=0)
+        logging.info("Socket created")
     except socket.error as err:
-        print("Error during creation of the socket")
-        print(f"Reason: {err}")
+        logging.error("Error during creation of the socket")
+        logging.info(f"Reason: {err}")
         sys.exit()
 
-    print('Socket created')
+    logging.info("Socket created")
 
     config = yaml.safe_load(open("config.yaml", "r"))
-    addr = (config['server']['host'], config['server']['port'])
-    name = config['name']
+    addr = (config["server"]["host"], config["server"]["port"])
+    name = config["name"]
 
     try:
         s.connect(addr)
     except ConnectionRefusedError:
-        manage_exception('Connection refused from the server')
+        manage_exception("Connection refused from the server")
     except TimeoutError:
         manage_exception("Timeout error during connection to the server")
 
     s.send(name.encode())
     msg = "x"
-    while msg != "end":
-        msg = input("Message: ")
+
+    t = Thread(target=print_msgs, kwargs={"s": s})
+    t.start()
+
+    while msg.lower() != "end":
+        msg = input(f"{name} < ")
         s.send(msg.encode())
 
     print("Connection closed")
@@ -38,9 +44,18 @@ def main():
 
 
 def manage_exception(arg0, target_host, target_port, arg3):
-    print(f"{arg0}{target_host}:{target_port}")
-    print(f"Details: {arg3}")
+    logging.warning(f"{arg0}{target_host}:{target_port}")
+    logging.warning(f"Details: {arg3}")
     sys.exit()
+
+
+def print_msgs(s):
+    while True:
+        try:
+            msg = s.recv(200).decode()
+            print(msg)
+        except OSError:
+            return
 
 
 main()
--- 2023-04-14/server.py
+++ 2023-04-14/server.py
@@ -1,3 +1,4 @@
+import logging
 import socket
 import sys
 from threading import Thread
@@ -6,11 +7,11 @@ from threading import Thread
 def main():
     try:
         # create a TCP socket (SOCK_STREAM)
-        s = socket.socket(family=socket.AF_INET,
-                          type=socket.SOCK_STREAM, proto=0)
+        s = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM, proto=0)
+        logging.info("Socket created")
     except socket.error as err:
-        print("Error during creation of the socket")
-        print(f"Reason: {err}")
+        logging.error("Error during creation of the socket")
+        logging.info(f"Reason: {err}")
         sys.exit()
 
     target_host = "0.0.0.0"
@@ -20,22 +21,39 @@ def main():
     s.listen(5)
     s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
+    c_sockets = []
     while True:
-        c_sock, c_addr = s.accept()
+        logging.info("Waiting for incoming connection...")
+        c_sock, _ = s.accept()
+        c_sockets.append(c_sock)
         c_name = c_sock.recv(200).decode()
-        t = Thread(target=client_management,
-                   kwargs={'name': c_name, 'sock': c_sock, 'addr': c_addr})
+        logging.info(f"{c_name} joined the chat")
+        t = Thread(
+            target=client_management,
+            kwargs={"name": c_name, "sock": c_sock, "sockets": c_sockets},
+        )
         t.start()
 
     s.close()
 
 
-def client_management(name, sock, addr):
+def client_management(name, sock, sockets):
     msg = "x"
-    while msg != "end":
+    while msg.lower() != "end":
         msg = sock.recv(200).decode()
-        print(f"{name}>", msg)
-    print(f"{name}> leaves the chat")
+        logging.info(msg)
+        if msg.lower() != "end":
+            for c_socket in sockets:
+                if c_socket != sock:
+                    try:
+                        c_socket.send(f"{name}> {msg}".encode())
+                    except OSError:
+                        pass
+    sockets.remove(sock)
+    msg = f"{name}> leaves the chat"
+    logging.warning(msg)
+    for c_socket in sockets:
+        c_socket.send(msg.encode())
     sock.close()
 
 
