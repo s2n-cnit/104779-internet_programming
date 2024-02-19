78321d721693a80e92bdfc0235e2f5e975882be2
Alex Carrega <contact@alexcarrega.com>
2024-02-19 16:11:17 +0100

    Minor changes

--- 2024-02-21/client.py
+++ 2024-02-21/client.py
@@ -5,25 +5,24 @@ import yaml
 
 
 def main():
-    try: 
+    try:
         # create a TCP socket (SOCK_STREAM)
-        s = socket.socket(family=socket.AF_INET,
-                          type=socket.SOCK_STREAM, proto=0)
+        s = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM, proto=0)
     except socket.error as err:
         print("Error during creation of the socket")
         print(f"Reason: {err}")
         sys.exit()
 
-    print('Socket created')
+    print("Socket created")
 
     config = yaml.safe_load(open("config.yaml", "r"))
-    addr = (config['server']['host'], config['server']['port'])
-    name = config['name']
+    addr = (config["server"]["host"], config["server"]["port"])
+    name = config["name"]
 
     try:
         s.connect(addr)
     except ConnectionRefusedError:
-        manage_exception('Connection refused from the server')
+        manage_exception("Connection refused from the server")
     except TimeoutError:
         manage_exception("Timeout error during connection to the server")
 
