fd723cd3f6c6857eb22f8d9671dca059a091c04a
Alex Carrega <contact@alexcarrega.com>
2024-02-27 15:34:34 +0100

    Removed useless tools

--- tools/setup.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/fish
-
-for user in (ls /home/)
-    if [ $user != "ubuntu" ]
-        cd /home/$user/Documents/
-        git clone https://github.com/s2n-cnit/104779-internet_programming.git 
-    end
-end
