88cb37da1ebf05fea33c96720820b5624ff63115
Alex Carrega <contact@alexcarrega.com>
2024-02-27 15:28:16 +0100

    Additional tools

--- tools/setup.sh
+++ tools/setup.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/fish
 
 for user in (ls /home/)
-    cd /home/$user/Documents/
-    git clone https://github.com/s2n-cnit/104779-internet_programming.git 
+    if [ $user != "ubuntu" ]
+        cd /home/$user/Documents/
+        git clone https://github.com/s2n-cnit/104779-internet_programming.git 
+    end
 end
