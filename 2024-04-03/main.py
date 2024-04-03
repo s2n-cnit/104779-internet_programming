e5a180d3fe88fd8f667b8a7ea92059f5027b36a2
Alex Carrega <contact@alexcarrega.com>
2024-04-03 16:25:44 +0200

    Lesson 3rd April 2024 changes

--- 2024-04-03/main.py
+++ 2024-04-03/main.py
@@ -1,15 +1,19 @@
 from typing import Union
-
+from datetime import datetime
 from fastapi import FastAPI
 
 app = FastAPI()
 
+@app.post("/x")
+@app.get("/test")
+def test_post():
+    return {"today": datetime.now() }
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
-
 @app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
+@app.get("/items")
+def read_item(item_id: Union[str, None] = "bye", q: Union[str, None] = "hello"):
     return {"item_id": item_id, "q": q}
