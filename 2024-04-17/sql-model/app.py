abd794204ea70e3ab8cd46c224a539140bad718f
Alex Carrega <contact@alexcarrega.com>
2024-04-14 21:54:15 +0200

    Lesson of 17th April 2024

--- 2024-04-17/sql-model/app.py
+++ 2024-04-17/sql-model/app.py
@@ -1,3 +1,8 @@
+import logging
+
 from fastapi import FastAPI
 
-app = FastAPI()
+app_name = "yacr"
+
+logger = logging.getLogger(app_name.lower())
+app = FastAPI(title=app_name.upper(), debug=True)
--- 2024-04-17/sql-model/main.py
+++ 2024-04-17/sql-model/main.py
@@ -2,4 +2,9 @@ import message  # noqa: F401
 import room  # noqa: F401
 import user  # noqa: F401
 import user_room  # noqa: F401
-from app import app  # noqa: F401
+import uvicorn
+from app import app, logger  # noqa: F401
+
+if __name__ == "__main__":
+    logger.info("CIao")
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
